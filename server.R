7de59b518c5f1ea8229ae0fac14a484fa9de1ffc
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-07-09 22:24:32 -0700

    collect most inputs into nicer nested boxes

--- server.R
+++ server.R
@@ -6,27 +6,28 @@ library(stringr)
 function(input, output) {
 
   output$contents <- renderDataTable({
-    uploaded_data()[1:20,]
-  })
+    uploaded_data()
+  }, options = list(
+    pageLength = 10
+  ))
 
   # input$file1 will be NULL initially. After the user selects
   # and uploads a file, head of that data file by default,
   # or all rows if selected, will be shown.
 
   uploaded_data <- reactive({
-    return(iris)
+    return(mtcars)
     req(input$file1)
 
-    # when reading semicolon separated files,
-    # having a comma separator causes `read.csv` to error
     tryCatch(
       {
-        df <- read.csv(input$file1$datapath,
-                       header = input$header,
-                       sep = input$sep,
-                       quote = input$quote,
-                       colClasses = "character",
-                       stringsAsFactors = FALSE)
+        df <- read.csv(
+          input$file1$datapath,
+          header = input$header,
+          sep = input$sep,
+          quote = input$quote,
+          colClasses = "character",
+          stringsAsFactors = FALSE)
       },
       error = function(e) {
         # return a safeError if a parsing error occurs
@@ -41,19 +42,23 @@ function(input, output) {
       collapsible = TRUE,
       collapsed = TRUE,
 
+      flowLayout(
         selectInput(str_glue("{name}_salter"), label = "Salt function",
                     choices = c("none", "Insert", "Substitute", "Replace", "Delete"),
                     selected = "none", multiple = FALSE),
-        selectInput(str_glue("{name}_shaker"), label = "Shaker",
-                    choices = available_shakers(), selected = "punctuation"),
-        sliderInput(str_glue("{name}_p"), label = "Proportion of values to salt",
-                    min = 0, max = 1, step = 0.1, value = 0.5),
-        sliderInput(str_glue("{name}_n"), label = "Proportion of characters to change",
-                    min = 0, max = 10, step = 1, value = 2),
-        sliderInput(str_glue("{name}_rep_p"), label = "Proportion of characters to change",
-                    min = 0, max = 1, step = 0.1, value = 0.5)
+        conditionalPanel(
+          condition = str_glue("input['{name}_salter'] != 'none'"),
+          selectInput(str_glue("{name}_shaker"), label = "Shaker",
+                      choices = available_shakers(), selected = "punctuation"),
+          sliderInput(str_glue("{name}_p"), label = "Proportion of values to salt",
+                      min = 0, max = 1, step = 0.1, value = 0.5),
+          sliderInput(str_glue("{name}_n"), label = "Proportion of characters to change",
+                      min = 0, max = 10, step = 1, value = 2),
+          sliderInput(str_glue("{name}_rep_p"), label = "Proportion of characters to change",
+                      min = 0, max = 1, step = 0.1, value = 0.5)
+        )
       )
-
+    )
   }
 
   salted_df <- reactive({
@@ -61,7 +66,6 @@ function(input, output) {
     df_names <- names(working_df)
     for (i in seq_along(df_names)) {
       coln <- df_names[i]
-      str(coln)
       salt_type <- input[[str_glue("{coln}_salter")]]
       if (salt_type == "Insert") {
         print("Inserting")
@@ -92,15 +96,18 @@ function(input, output) {
   })
 
   output$salted_table <- renderDataTable({
-    salted_df()[1:20,]
-  })
+    salted_df()
+  }, options = list(
+    pageLength = 10
+  ))
 
   output$salt_opts <- renderUI({
     lapply(names(uploaded_data()), column_ui)
   })
 
+
   output$download_salted <- downloadHandler(
-    filename = function() "salted_data.csv",
+    filename = function() str_glue("salted_{input$file1$name}"),
     content = function(file) write.csv(salted_df(), file, row.names = FALSE)
   )
 }
--- ui.R
+++ ui.R
@@ -10,35 +10,37 @@ upload_menu <- menuItem(
 
 upload_tab <- tabItem(
   tabName = "upload_tab",
-  box("CSV parsing options", width = 3,
+  fluidRow(
+    box("Import data", width = 12,
 
-  # Input: Select a file ----
-  fileInput("file1", "Choose CSV File",
-            multiple = FALSE,
-            accept = c("text/csv",
-                       "text/comma-separated-values,text/plain",
-                       ".csv")),
+      flowLayout(
+          # Input: Select a file ----
+          fileInput("file1", "Choose CSV File",
+                    multiple = FALSE,
+                    accept = c("text/csv",
+                               "text/comma-separated-values,text/plain",
+                               ".csv")),
 
-  # Horizontal line ----
-  tags$hr(),
 
-  # Input: Checkbox if file has header ----
-  checkboxInput("header", "Header", TRUE),
+          # Input: Checkbox if file has header ----
+          checkboxInput("header", "Header", TRUE),
 
-  # Input: Select separator ----
-  radioButtons("sep", "Separator",
-               choices = c(Comma = ",",
-                           Semicolon = ";",
-                           Tab = "\t"),
-               selected = ","),
+          # Input: Select separator ----
+          radioButtons("sep", "Separator",
+                       choices = c(Comma = ",",
+                                   Semicolon = ";",
+                                   Tab = "\t"),
+                       selected = ","),
 
-  # Input: Select quotes ----
-  radioButtons("quote", "Quote",
-               choices = c(None = "",
-                           "Double Quote" = '"',
-                           "Single Quote" = "'"),
-               selected = '"')),
-  box("Data preview", width = 9, dataTableOutput("contents"))
+          # Input: Select quotes ----
+          radioButtons("quote", "Quote",
+                       choices = c(None = "",
+                                   "Double Quote" = '"',
+                                   "Single Quote" = "'"),
+                       selected = '"')
+      ))),
+
+  fluidRow(box("Data preview", width = 12, dataTableOutput("contents")))
 )
 
 salt_menu <- menuItem(
@@ -49,11 +51,17 @@ salt_menu <- menuItem(
 salt_tab <- tabItem(
   tabName = "salt_tab",
   title = "Salting Options",
-  uiOutput("salt_opts"),
-  box(title = "Data preview", width = 12,
-      downloadButton("download_salted", label = "Download salted data"),
-      hr(),
-      dataTableOutput("salted_table"))
+  fluidRow(
+    box(title = "Salting Options", width = 12,
+        p("To salt a column with erroneous data, open up its control panel below and select a salting method. Additional options will then appear to fine tune how the salting works. Consult the data preview below. When you are satisfied, click on the download button to get your salted file back as a CSV"),
+        hr(),
+        downloadButton("download_salted", label = "Download salted data"),
+    hr(),
+
+    uiOutput("salt_opts"))),
+  fluidRow(box(title = "Data preview", width = 12,
+               hr(),
+               dataTableOutput("salted_table")))
 )
 
 sidebar <- dashboardSidebar(
