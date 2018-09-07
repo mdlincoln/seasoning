cc5a0358eaf82a4fcf3cf3d75c816701b2a42fbd
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-09-06 22:22:10 -0400

    use base data.frame rather than dplyr version

--- global.R
+++ global.R
@@ -4,7 +4,7 @@ library(purrr)
 
 n <- 30
 
-starter_data <- data_frame(
+starter_data <- data.frame(
   name = ch_name(n),
   date = map_chr(ch_date_time(n), function(x) str_sub(as.character(x), 1, 10)),
   phonenubmers = ch_phone_number(n),
